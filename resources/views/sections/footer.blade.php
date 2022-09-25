f4b94a53f58070a283a4ae0c352b179d272f7761
Mateusz Misiak <MateuszMisiak1@gmail.com>
2022-09-25 16:47:02 +0200

    reinitialize files

---
+++ resources/views/sections/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="content-info p-4">
+  <div class="flex space-x-4">
+    @php(dynamic_sidebar('sidebar-footer'))
+    @php(dynamic_sidebar('sidebar-footer-1'))
+    @php(dynamic_sidebar('sidebar-footer-2'))
+    @php(dynamic_sidebar('sidebar-footer-3'))
+  </div>
+</footer>
